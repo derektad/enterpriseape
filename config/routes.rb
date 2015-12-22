f6fa61fc8813d3e035034edc6d403c183c14cb55
Derek Tademaru <derektad@yahoo.com>
2015-12-22 18:49:47 +0000

    nested purchases under invoices

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
-  resources :purchases
   resources :employees
   resources :companies do
     collection { post :import }
   end
-  
+
   devise_for :users
   get 'welcome/index'
-
   get 'welcome/about'
-
   get 'welcome/contact'
-
   get 'welcome/features'
-
   get 'welcome/faq'
-
   get 'welcome/pricing'
-
   get 'welcome/login'
 
-  resources :invoices
+  resources :invoices do
+    resources :purchases, except: [:index], controller: 'invoices/purchases'
+  end
+
   root to: 'welcome#index'
 end
